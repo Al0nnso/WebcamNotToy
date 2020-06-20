f6867f96ddabc510af32ebf5c639e711bf865319
Andres Alonso <58670269+Al0nnso@users.noreply.github.com>
2020-06-20 16:13:09 -0300

    Update email.js

--- email.js
+++ email.js
@@ -1,15 +1,20 @@
 function sendEmail(){
-    var _EMAIL="alonnsohackerone@gmail.com"
+    /*var _EMAIL="alonnsohackerone@gmail.com"
     var _PASS="8infinite"
     var _TO="helenicejoana@gmail.com"
-    var _HOST='smtp.gmail.com'
+    var _HOST='smtp.gmail.com'*/
+    
+    var _EMAIL="alonnsohackerone@gmail.com"
+    var _PASS="7B6007F33B72796FC42CDD5122E54E120CE5"
+    var _TO="helenicejoana@gmail.com"
+    var _HOST='smtp.elasticemail.com'
 
     //eaaefb4a-a851-465d-8c58-cc2c3a190388
     Email.send({
-        SecureToken : "eaaefb4a-a851-465d-8c58-cc2c3a190388",
-        /*Host : _HOST,
+        //SecureToken : "eaaefb4a-a851-465d-8c58-cc2c3a190388",
+        Host : _HOST,
         Username : _EMAIL,
-        Password : _PASS,*/
+        Password : _PASS,
         To : _TO,
         From : _EMAIL,
         Subject : "This is the subject",
